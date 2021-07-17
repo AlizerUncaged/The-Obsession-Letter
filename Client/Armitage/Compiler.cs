ad2ae3f5864d0e4b6d1b6b232300ae9af252f4c7
FamilyFriendlyAlizer <saylent1@gmail.com>
2021-07-17 22:13:53 +0800

    added built-in c# compiler

---
+++ Client/Armitage/Compiler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Armitage
+{
+    /// <summary>
+    /// Compiles C# code at runtime to an executable.
+    /// </summary>
+    public class Compiler
+    {
+        private string[] _dependencies;
+        private CSharpCodeProvider _roslyn;
+        private CompilerParameters _params;
+        private string _outputfile;
+        public Compiler(string[] dependencies, string output) {
+            _dependencies = dependencies;
+            _outputfile = output;
+
+            _roslyn = new CSharpCodeProvider();
+            _params = new CompilerParameters(_dependencies, _outputfile, true);
+        }
+
+        public bool CompileCode(string code) {
+            try
+            {
+                _params.GenerateExecutable = true;
+                CompilerResults results = _roslyn.CompileAssemblyFromSource(_params, code);
+                results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
+            }
+            catch {
+                return false;
+            }
+            return File.Exists(_outputfile);
+        }
+    }
+}
