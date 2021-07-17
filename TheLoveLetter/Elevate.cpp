d05c49c1f445d8068ab3cca9ea48985f23c5b654
FamilyFriendlyAlizer <saylent1@gmail.com>
2021-07-17 12:10:02 +0800

    elevation outline

---
+++ TheLoveLetter/Elevate.cpp
@@ -0,0 +1,8 @@
+/// Working methods, should work from Windows 7 to latest OS (Windows 11 as of writing this)
+/// From https://github.com/hfiref0x/UACME
+/// 41.
+/// 43. 
+/// 53.
+/// 59.
+
+#include "Elevate.h"
