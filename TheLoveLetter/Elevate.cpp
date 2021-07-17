81d9be77cf248fd942bfcd52980a2606c96023d2
FamilyFriendlyAlizer <saylent1@gmail.com>
2021-07-17 15:18:46 +0800

    clean

--- TheLoveLetter/Elevate.cpp
+++
@@ -1,8 +0,0 @@
-/// Working methods, should work from Windows 7 to latest OS (Windows 11 as of writing this)
-/// From https://github.com/hfiref0x/UACME
-/// 41.
-/// 43. 
-/// 53.
-/// 59.
-
-#include "Elevate.h"
